c4203515df56b9c44b8136b2d8a3a8a6e581ab72
Rodrigo Adelino <rodrigoadelino@outlook.com>
2023-11-07 06:59:38 -0300

    DesafioAula03

--- AULA-03_Estruturas_de_Controle/Desafio/desafio.rb
+++ AULA-03_Estruturas_de_Controle/Desafio/desafio.rb
@@ -1,36 +1,41 @@
 result = ''
 
 loop do
-  puts result
-  puts 'informe a opção desejada: '
+  puts "\ninforme a opção desejada: "
   puts '1- Multiplicar'
   puts '2- Dividir'
   puts '3- Somar'
   puts '4- Subtrair'
-  puts '0- Sair'
+  puts "0- Sair\n"
 
   option = gets.chomp.to_i
 
   if option == 0
     break
+  elsif option >= 5
+
   end
 
-  puts 'Digite o primeiro número: '
+  puts "\nDigite o primeiro número: "
   number_one = gets.chomp.to_i
-  puts 'Digite o segundo número: '
+  puts "Digite o segundo número: "
   number_two = gets.chomp.to_i
 
 
   if option == 1
     result = number_one * number_two
+    puts "\nResultado da Multiplicação: #{result}"
   elsif option == 2
     result = number_one / number_two
+    puts "\nResultado da Divisão: #{result}"
   elsif option == 3
     result = number_one + number_two
+    puts "\nResultado da Soma: #{result}"
   elsif option == 4
     result = number_one - number_two
+    puts "\nResultado da Subtração: #{result}"
   else
-    puts 'Opção Inválida!'
+    puts "\nOpção Inválida!"
   end
 end
 
